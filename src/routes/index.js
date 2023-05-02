3242a37fa77eaab4887a9a6b79d04fb29ab44115
Cong Le <lecong97777@gmail.com>
2023-05-02 15:28:15 +0900

    LAyout

---
+++ src/routes/index.js
@@ -0,0 +1,39 @@
+import Home from '~/pages/Home'
+//Layouts
+import { HeaderOnly } from '~/components/Layout'
+
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search'
+
+//Authentication not required
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
+    },
+]
+
+// Authentication required
+const privateRoutes = []
+
+export { publicRoutes, privateRoutes }
